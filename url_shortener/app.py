6490e9ffd27639fc784d3afdf9e72368e45cff1b
Anu1996rag <anuragpatils1996@gmail.com>
2023-11-10 18:51:39 +0530

    added new endpoint to store details of the url to the db

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+import secrets
+import string
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import schemas, models, database
+from .database import get_db
+
+# create database
+models.base.metadata.create_all(database.engine)
+
 
 app = FastAPI()
+
+chars = string.ascii_letters
 
 
 @app.get("/")
 def home():
     return "welcome to URL shortener API"
+
+
+@app.post("/url")
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+
+    db_url = models.URL(target_url=url.target_url,
+                        key=key,
+                        secret_key=secret_key)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
