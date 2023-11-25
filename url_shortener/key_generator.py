2c1cd935bfbb5792063964cfa910d1609f012110
Anu1996rag <anuragpatils1996@gmail.com>
2023-11-25 12:19:39 +0530

    added method for generating unique key everytime it is hit

--- url_shortener/key_generator.py
+++ url_shortener/key_generator.py
@@ -1,8 +1,20 @@
 import secrets
 import string
+from sqlalchemy.orm import Session
+
+from . import crud
+
 
 chars = string.ascii_letters + string.digits
 
 
 def create_random_key(length: int = 5) -> str:
     return "".join(secrets.choice(chars) for _ in range(length))
+
+
+def create_unique_random_key(db: Session) -> str:
+    key_generated = create_random_key()
+    while crud.get_db_url_by_key(db, key_generated):
+        key_generated = create_random_key()
+    return key_generated
+
