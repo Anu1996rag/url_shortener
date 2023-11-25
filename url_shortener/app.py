8f85b91972631bf9b49eef7ac010e5363cce3ebb
Anu1996rag <anuragpatils1996@gmail.com>
2023-11-25 12:21:17 +0530

    updated the file with new functions for crud operations

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,19 +1,14 @@
-import secrets
-import string
-from fastapi import FastAPI, Depends, Request, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from . import schemas, models, database
+from . import schemas, models, database, crud
 from .database import get_db
 
 # create database
 models.base.metadata.create_all(database.engine)
 
-
 app = FastAPI()
-
-chars = string.ascii_letters
 
 
 @app.get("/")
@@ -23,21 +18,14 @@ def home():
 
 @app.post("/url")
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-
-    db_url = models.URL(target_url=url.target_url,
-                        key=key,
-                        secret_key=secret_key)
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
+    db_url = crud.create_db_url(db, url)
+    db_url.url = url.key
     return db_url
 
 
 @app.get("/{url_key}")
-def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
-    db_url = (db.query(models.URL).filter(models.URL.key == url_key, models.URL.is_active).first())
+def forward_to_target_url(url_key: str, db: Session = Depends(get_db)):
+    db_url = crud.get_db_url_by_key(db, url_key)
     if db_url:
         return RedirectResponse(url=db_url.target_url)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Request URL not found or the key is invalid")
